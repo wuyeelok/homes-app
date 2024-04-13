35739de9a4aab3e66da4f7330a87f7a3fcafd6cc
wuyeelok <wuyeelok@gmail.com>
2024-04-13 14:03:48 +0800

    Work on hme component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Serach</button>
+      </form>
+    </section>
+    <section class="results"></section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
