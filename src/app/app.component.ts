173eccf97cdb21d978d7a8bd49c6735fc36ce687
wuyeelok <wuyeelok@gmail.com>
2024-04-13 13:14:19 +0800

    Upate text on template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  template: `<h1>Hello Universe Yo!</h1>`,
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
