dfb28b6af905135362ad9c548a53e78be6865f57
wuyeelok <wuyeelok@gmail.com>
2024-04-13 16:37:48 +0800

    Route: Pass and retrive info using ActivatedRoute

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
-  styleUrls: ['./details.component.css']
+  template: ` <p>details works! {{ housingLocationId }}</p> `,
+  styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+  }
 }
