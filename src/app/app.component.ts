1e862e30e4cb60a81031b220b004f69ecd526392
wuyeelok <wuyeelok@gmail.com>
2024-04-13 13:57:11 +0800

    Create a logo in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from "@angular/core";
 @Component({
   standalone: true,
   selector: "app-root",
-  template: `<h1>Hello Universe Yo!</h1>`,
+  template: `<main>
+    <header class="brad-name">
+      <img
+        class="bradn-logo"
+        src="/assets/logo.svg"
+        alt="logo"
+        aria-hidden="true"
+      />
+    </header>
+  </main>`,
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
