7276db2694d9cd550269bbc59ed9b12e5182d382
wuyeelok <wuyeelok@gmail.com>
2024-04-13 18:16:30 +0800

    Get data from server and output to the DOM

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -60,8 +60,12 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    // this.housingLocation =
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
   }
   submitApplication() {
     this.housingService.submitApplication(
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,6 +29,10 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+      });
   }
 }
